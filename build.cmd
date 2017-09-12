65210a7536addc5e686b4c60e620f2e7b1db6bcb
Burke Azbill <burkeazbill@gmail.com>
2017-09-12 09:53:52 +0200

    Update build.cmd to include copy src files Signed-off-by: Burke Azbill <burkeazbill@users.noreply.github.com>

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 git clone https://github.com/vmware/clarity-seed.git
+cp -R src clarity-seed
 docker build -t "node-angular:6.11.3-1.3.2" .
 docker run -v $PWD/clarity-seed:/project --name clarity-seed -p 4200:4200 -d node-angular:6.11.3-1.3.2
